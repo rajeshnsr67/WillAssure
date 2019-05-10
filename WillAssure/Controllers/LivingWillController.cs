236b327f3dc44789ba01ba0dc8560a0bbd0b3df9
imran sayyed <imran528@hotmail.com>
2019-05-11 01:16:22 +0530

    ww h

---
+++ WillAssure/Controllers/LivingWillController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WillAssure.Controllers
+{
+    public class LivingWillController : Controller
+    {
+        // GET: LivingWill
+        public ActionResult LivingWillIndex()
+        {
+
+
+
+
+            return View("~/Views/LivingWill/LivingWillPageContent.cshtml");
+        }
+    }
+}
